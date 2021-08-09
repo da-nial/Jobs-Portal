74bba2fcd8a554ad9db548b81495dadb4ae9aa72
Ali Javani <aj.ali.javan@gmail.com>
2021-08-09 11:32:42 +0430

    Sign up

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,13 @@
+from django.conf import settings
 from django.shortcuts import render
+from django.views.generic import CreateView
 
-# Create your views here.
+from authentication.forms import UserForm
+from authentication.models import CustomUser
+
+
+class RegisterView(CreateView):
+    model = CustomUser
+    template_name = 'register.html'
+    form_class = UserForm
+    success_url = settings.LOGIN_URL
--- website/urls.py
+++ website/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
 from django.conf.urls.static import static
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('authentication.urls')),
 ]
 
 if settings.DEBUG:
