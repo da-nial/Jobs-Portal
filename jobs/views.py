72a88f2bbef30bb843d80ebaef1993c3122f45ab
Sara Tavakoli <saratvk1377@gmail.com>
2021-08-11 10:42:17 +0430

    company details

--- jobs/views.py
+++ jobs/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views import generic
-from .models import JobOffer, UserProfile, EducationalBackground
+
+from .models import JobOffer, UserProfile, Company
 
 
 class JobOffersView(generic.DetailView):
@@ -13,4 +14,9 @@ class JobOffersView(generic.DetailView):
 class UserProfileView(generic.DetailView):
     model = UserProfile
     template_name = 'user_profile.html'
+
+
+class CompanyView(generic.DetailView):
+    template_name = 'company_details.html'
+    model = Company
 
