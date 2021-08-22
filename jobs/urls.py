4156f7e000e903c318903efdc6b20ea40042b91f
Sara Tavakoli <saratvk1377@gmail.com>
2021-08-22 13:07:48 +0430

    Profile page security: remove pk from profile url

--- jobs/urls.py
+++ jobs/urls.py
@@ -6,8 +6,8 @@ app_name = 'jobs'
 urlpatterns = [
     # ex: /job_offers/5
     path('job_offers/<int:pk>/', views.JobOffersView.as_view(), name='job_offers'),
-    path('profile/<int:pk>/', views.UserProfileView.as_view(), name='user_profile'),
     path('', views.MainView.as_view(), name='main'),
+    path('profile/', views.UserProfileView.as_view(), name='user_profile'),
     path('company/<int:pk>/', views.CompanyView.as_view(), name='company'),
     # ex: /edit_profile
     path('edit_profile/', views.edit_profile_view, name='edit_profile'),
