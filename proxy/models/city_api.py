671ea8deb1443d253703c70a8dda746102ee4162
Mohammad Nourmohammadi <2013nourmohammadi@gmail.com>
2021-08-23 10:41:36 +0430

    get cities api by try except

--- proxy/models/city_api.py
+++ proxy/models/city_api.py
@@ -8,13 +8,20 @@ class CitiesProxy:
     def load_data(self):
         full_data = cache.get("cities_data", None)
         if full_data is None:
-            response = requests.get("https://api.divar.ir/v8/places/cities")
-            full_data = response.json()
-            cache.set("cities_data", full_data, 60 * 60 * 24)
+            try:
+                response = requests.get("https://api.divar.ir/v8/places/cities",  timeout=1)
+                full_data = response.json()
+                cache.set("cities_data", full_data, 60 * 60 * 24)
+            except requests.ConnectionError:
+                full_data = None
+            except requests.ReadTimeout:
+                full_data = None
         return full_data
 
     def get_city_name_tuple(self):
         full_data = self.load_data()
+        if full_data is None:
+            return []
         city_name_tuple = [(city["name"], city["name"]) for city in full_data["cities"]]
         return city_name_tuple
 
@@ -23,5 +30,3 @@ class CitiesProxy:
         if CitiesProxy.instance is None:
             CitiesProxy.instance = CitiesProxy()
         return CitiesProxy.instance
-
-
