9859078978add3e1e5a1151e2651f143f086942c
Mohammad Nourmohammadi <2013nourmohammadi@gmail.com>
2021-08-31 11:55:12 +0430

    Forget password

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
-
 from django.contrib.auth.views import LogoutView
-
 from . import views
 
 app_name = 'auth'
@@ -11,4 +9,15 @@ urlpatterns = [
     path('send-email-verification/', views.send_email_verification, name='send-email-verification'),
     path('verify/<str:token>/', views.verify_email, name='verify'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('password-reset/', views.CustomPasswordResetView.as_view(),
+         name='password_reset'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         views.CustomPasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('password-reset-done/',
+         views.CustomPasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    path('password-reset-complete/',
+         views.CustomPasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
 ]
